e3a675ea3cd886d0849729000a722f8d9634605f
Md Ashiqur Rahman <md.ashik.ar@gmail.com>
2023-03-01 13:01:07 +0600

    feat: added get route to gen img

--- src/routes/v1/image.route.js
+++ src/routes/v1/image.route.js
@@ -4,5 +4,5 @@ const router = express.Router();
 const imageController = require('../../controllers/image.controller');
 
 router.post('/', imageController.generateImageViaKonva);
-
+router.get('/', imageController.generateImageViaKonva);
 module.exports = router;
